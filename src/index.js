6e2c31c3d5449e5ce75a77867a8eb9881975757a
Matthew Conlen <mc@mathisonian.com>
2015-09-25 18:51:43 -0400

    click events and recursive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  VictoryTree: require("./components/victory-tree")
+  VictoryTree: require("./components/victory-tree"),
+  VictoryTreeNode: require("./components/victory-tree-node")
 };
